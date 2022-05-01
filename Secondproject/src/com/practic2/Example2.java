c15bb744ee46375c4e6790f3c0c8756ddae25f33
Mimi1973 <etse.haile2@gmail.com>
2022-05-01 07:44:29 -0400

    commit 2

---
+++ Secondproject/src/com/practic2/Example2.java
@@ -0,0 +1,11 @@
+package com.practic2;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		System.out.println("It is a beautiful day");
+		// TODO Auto-generated method stub
+
+	}
+
+}

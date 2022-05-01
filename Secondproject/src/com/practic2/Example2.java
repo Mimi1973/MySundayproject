98e9eccfd73d9883fa110764e88c0904785e1e87
Mimicraft <100546395+Mimi1973@users.noreply.github.com>
2022-05-01 07:55:38 -0400

    Update Example2.java
    update file

--- Secondproject/src/com/practic2/Example2.java
+++ Secondproject/src/com/practic2/Example2.java
@@ -4,6 +4,7 @@ public class Example2 {
 
 	public static void main(String[] args) {
 		System.out.println("It is a beautiful day");
+		System.out.println "It is Sunday");
 		// TODO Auto-generated method stub
 
 	}
